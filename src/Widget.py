da05b04d427b0ff8c5898dedea3e09fb18c748aa
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2025-11-07 23:38:05 +0100

    container widget

--- src/Widget.py
+++ src/Widget.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
-from typing import TypeVar, Type
-T = TypeVar("T", bound="Widget")
+from typing import TypeVar, Type, final
+from abc import ABC, abstractmethod
+from importlib import import_module
 
 from pygame import Surface, Color, SRCALPHA
 
 from Event import Event,EventType
 
-class Widget:
+T = TypeVar("T", bound="Widget")
+
+class Widget(ABC):
     id: int
     x: int
     y: int
@@ -18,7 +21,7 @@ class Widget:
 
     draw_surface: Surface
 
-    childs: dict[int,Widget] = {}
+    widget_surface: Surface
 
     def __init__(self, id:int, x:int, y:int, width:int, height:int, color: Color, parent:None|Widget, draw_surface: Surface) -> None:
         self.id = id
@@ -29,6 +32,7 @@ class Widget:
         self.color = color
         self.parent = parent
         self.draw_surface = draw_surface
+        self.widget_surface = Surface( (width, height), SRCALPHA )
 
         for event_type in EventType:
             if event_type.value == None: continue
@@ -39,31 +43,16 @@ class Widget:
             self.__setattr__(method_name, on_event)                               # add the method to the object
         self.on_event( Event( EventType.INIT_EVENT, {"id": id}) )                 # on init event call
 
-    def append_child(self, widget: Widget) -> None:
-        """Add a child to this widget"""
-        self.childs[widget.id] = widget
-    
-    def remove_child(self, widget_id: int) -> None:
-        """Remove a child from this widget"""
-        self.childs.pop(widget_id)
-
     def show(self) -> None:
         """Show to the draw surface the widget"""
-        to_draw: Surface = Surface( (self.width,self.height), SRCALPHA )
-        to_draw.fill(self.color)
-        self.draw_surface.blit(to_draw, (self.x,self.y))
-        for child in self.childs.values():
-            child.show()
+        self.widget_surface.fill(self.color)
+        self.draw_surface.blit(self.widget_surface, (self.x,self.y))
 
     @classmethod
+    @abstractmethod
     def from_dict(cls: Type[T], id:int, parent:None|Widget, draw_surface:Surface, values:dict) -> T:
         """Generate a Widget from a JSON data (as dict)"""
-        x:int = values.get("x", 0) + parent.x if parent != None else 0
-        y:int = values.get("y", 0) + parent.y if parent != None else 0
-        width:int = values.get("width", 0)
-        height:int = values.get("height", 0)
-        color:Color = Color(values.get("background", "#FFFFFF00"))
-        return cls(id,x,y,width,height,color,parent,draw_surface)
+        pass
 
     def on_event(self, event:Event) -> None:
         """Trigger an event"""
@@ -72,3 +61,50 @@ class Widget:
         event_method = self.__getattribute__(event_method_name)                    # always exists (generate at the init) -> dev is supposed to do his job well so this method is always right writted
         if not event_method(event) and self.parent != None:                        # propagate if "asked" and possible
             self.parent.on_event(event)
+
+class ContainerWidget(Widget):
+    content: list[Widget]
+    children: dict[int, Widget]
+
+    def __init__(self, id: int, x: int, y: int, width: int, height: int, color: Color, parent: None | Widget, draw_surface: Surface) -> None:
+        super().__init__(id, x, y, width, height, color, parent, draw_surface)
+        self.content = []
+        self.children = {}
+    
+    @classmethod
+    @final
+    def generate_children(cls, container: ContainerWidget, children_data: list[dict]):
+        """Generate the list of children of the Widget"""
+        children: list[Widget] = []
+        child_number: int = 1
+        for child_data in children_data:
+            class_type = child_data["type"]
+            try:
+                child_module = import_module("widgets." + class_type, "src")        # Import the module where the Widget is supposed to be
+                child_class = getattr(child_module, class_type)                     # Get the class of the Widget
+                if child_class == None: continue                                    # If the class does not exists then it cannot be a Widget
+                child = child_class.from_dict(                                      # Generate the child widget
+                    container.id * 100 + child_number,
+                    container,
+                    container.widget_surface,
+                    child_data
+                )
+                children.append(child)
+                child_number += 1
+            except:
+                pass
+        container.append_child(*children)
+
+    def append_child(self, *widgets: Widget) -> None:
+        """Add a child to this widget"""
+        for widget in widgets:
+            self.children[widget.id] = widget
+    
+    def remove_child(self, widget_id: int) -> None:
+        """Remove a child from this widget"""
+        self.children.pop(widget_id)
+    
+    def show(self) -> None:
+        super().show()
+        for child in self.children.values():
+            child.show()
