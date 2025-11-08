19f852afa6243d06127a9d67f19cef6001145805
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2025-11-08 22:34:47 +0100

    TextWidget' show method

--- src/widgets/TextWidget.py
+++ src/widgets/TextWidget.py
@@ -1,4 +1,4 @@
-from pygame import Color, Surface, draw
+from pygame import Color, Surface, font
 from Widget import *
 from Event import *
 
@@ -6,6 +6,7 @@ from typing import Any
 
 class TextWidget(Widget):
     text: str
+    text_color: Color
     font_file: str|None
     font_size: int
 
@@ -15,6 +16,22 @@ class TextWidget(Widget):
         self.text_color = text_color
         self.font_file = font_file
         self.font_size = font_size
+    
+    def show(self) -> None:
+        self.widget_surface.fill(self.background_color)
+        text_font: font.Font = font.Font(self.font_file, self.font_size)
+        text_surface: Surface = text_font.render(self.text, False, self.text_color)
+        self.widget_surface.blit(
+            text_surface,
+            (
+                self.widget_surface.get_width() // 2 - text_surface.get_width() // 2,
+                self.widget_surface.get_height() // 2 - text_surface.get_height() // 2
+            )
+        )
+        self.draw_surface.blit(
+            self.widget_surface,
+            (self.x, self.y)
+        )
 
     @classmethod
     def from_dict(cls: type[T], id: int, parent: None | Widget, draw_surface: Surface, values: dict) -> T:
